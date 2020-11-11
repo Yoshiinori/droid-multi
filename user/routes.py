2aa4aefe2d99f9ff447e70fbf38fa62152383cd3
Yoshiinori <yoshiinoridev@gmail.com>
2020-11-11 04:08:00 +0000

    added security on verify to avoid duplicates

--- user/routes.py
+++ user/routes.py
@@ -14,7 +14,7 @@ db = client.droidmulti
 collection = db.test
 
 
-@user.route('/<username>')
+@user.route('/<username>/')
 def get_user(username):
     user_stats = collection.find_one({'username': username})
     verify = False
@@ -54,33 +54,40 @@ def verify():
         return redirect('/')
 
 
-@user.route('/verify/<ibancho_id>')
+@user.route('/verify/<ibancho_id>/')
 def verify_user(ibancho_id):
     if 'username' in session:
         username = session['username']
         user_stats = {'username': username}
-        try:
-            get_user = requests.get(
-                f'http://ops.dgsrz.com/profile.php?uid={ibancho_id}')
-            soup = BeautifulSoup(get_user.text, 'html.parser')
-            ibancho_username = soup.find(class_='h3 m-t-xs m-b-xs').get_text()
-            latest_map = soup.find(class_='list-group-item').find(
-                class_='block').get_text()
-            if latest_map == 'Mykal Williams - Splatoon - Splattack (Remix) (foxybus) [Easy]':
-                data = {
-                    '$set': {
-                        'ibancho_id': ibancho_id,
-                        'ibancho_username': ibancho_username
+        get_stats = collection.find_one({'username': username})
+        if get_stats['ibancho_id'] == 'Not Verified':
+            try:
+                get_user = requests.get(
+                    f'http://ops.dgsrz.com/profile.php?uid={ibancho_id}')
+                soup = BeautifulSoup(get_user.text, 'html.parser')
+                ibancho_username = soup.find(class_='h3 m-t-xs m-b-xs').get_text()
+                latest_map = soup.find(class_='list-group-item').find(
+                    class_='block').get_text()
+                if (latest_map == 'Mykal Williams - Splatoon - Splattack (Remix) (foxybus) [Easy]') and (get_stats[ibancho_username] == None):
+                    data = {
+                        '$set': {
+                            'ibancho_id': ibancho_id,
+                            'ibancho_username': ibancho_username
+                        }
                     }
-                }
-                collection.update_one(user_stats, data)
-                return 'you are now verified'
-               
-            else:
-                flash(
-                    'It\'s either you played the wrong map or you used someone\'s id'
-                )
-                return redirect('/user/verify')
-        except:
-            flash('User not found')
-            return redirect('/user/verify/')
+                    collection.update_one(user_stats, data)
+                    return 'you are now verified'
+                  
+                else:
+                    flash(
+                        'The id is taken'
+                    )
+                    return redirect('/user/verify')
+            except:
+                flash('The id you have entered has not played the map, or you gave the wrong id,or you used someones id, or the id has been verified')
+                return redirect('/user/verify/')
+        else:
+          return redirect('/')
+    else:
+      return redirect('/')
+
