37c29fd18f17746dc69ccb8f68f4f9f432eee5d3
Yoshiinori <yoshiinoridev@gmail.com>
2020-11-11 02:59:17 +0000

    added a verification system

--- user/routes.py
+++ user/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, redirect, session, flash
+from flask import Blueprint, render_template, redirect, session, flash, request
 from pymongo import MongoClient
 import os
+from bs4 import BeautifulSoup
+import requests
 
 user = Blueprint('user', __name__, template_folder='templates')
 
@@ -15,12 +17,70 @@ collection = db.test
 @user.route('/<username>')
 def get_user(username):
     user_stats = collection.find_one({'username': username})
+    verify = False
     if user_stats != None:
-      if 'username' in session:
-        return render_template(
-            'user.html', username=username, user_stats=user_stats)
-      else:
-          return render_template(
-            'user.html',username=username, user_stats=user_stats, edit='true')
+        if 'username' in session:
+            user_in_session = session['username']
+            if user_in_session == username:
+                if user_stats['ibancho_id'] == 'Not Verified':
+                    return render_template(
+                        'user.html',
+                        username=username,
+                        user_stats=user_stats,
+                        verify=True)
+                else:
+                    return render_template(
+                        'user.html', username=username, user_stats=user_stats)
+            else:
+                return render_template(
+                    'user.html', username=username, user_stats=user_stats)
+        else:
+            return render_template(
+                'user.html', username=username, user_stats=user_stats)
     else:
         return 'no user found'
+
+
+@user.route('/verify/')
+def verify():
+    if 'username' in session:
+        username = session['username']
+        user_stats = collection.find_one({'username': username})
+        if user_stats['ibancho_id'] != 'Not Verified':
+            return redirect(f'/user/{username}/')
+        else:
+            return render_template('verify.html', username=username)
+    else:
+        return redirect('/')
+
+
+@user.route('/verify/<ibancho_id>')
+def verify_user(ibancho_id):
+    if 'username' in session:
+        username = session['username']
+        user_stats = {'username': username}
+        try:
+            get_user = requests.get(
+                f'http://ops.dgsrz.com/profile.php?uid={ibancho_id}')
+            soup = BeautifulSoup(get_user.text, 'html.parser')
+            ibancho_username = soup.find(class_='h3 m-t-xs m-b-xs').get_text()
+            latest_map = soup.find(class_='list-group-item').find(
+                class_='block').get_text()
+            if latest_map == 'Mykal Williams - Splatoon - Splattack (Remix) (foxybus) [Easy]':
+                data = {
+                    '$set': {
+                        'ibancho_id': ibancho_id,
+                        'ibancho_username': ibancho_username
+                    }
+                }
+                collection.update_one(user_stats, data)
+                return 'you are now verified'
+               
+            else:
+                flash(
+                    'It\'s either you played the wrong map or you used someone\'s id'
+                )
+                return redirect('/user/verify')
+        except:
+            flash('User not found')
+            return redirect('/user/verify/')
